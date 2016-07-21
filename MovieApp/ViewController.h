eab244dabf1f84c1c5d642f3e3eaaaca4611e269
Geofrey Nyabuto <mwambage@gmail.com>
2016-07-21 09:24:51 +0300

    Initial Commit

---
+++ MovieApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MovieApp
+//
+//  Created by ilabadmin on 7/21/16.
+//  Copyright (c) 2016 softikoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MovieApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MovieApp
+//
+//  Created by ilabadmin on 7/21/16.
+//  Copyright (c) 2016 softikoda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
